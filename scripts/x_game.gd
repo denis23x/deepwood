5f1449ba5a8b06fab6b005111e6c92583f248e27
denis23x <damage.23x@gmail.com>
2025-01-14 15:48:14 +0700

    Update

--- scripts/x_game.gd
+++ scripts/x_game.gd
@@ -6,12 +6,15 @@ extends Node2D
 @onready var label_2: Label = $Labels/Label2
 @onready var label_3: Label = $Labels/Label3
 @onready var canvas_layer: CanvasLayer = $Vignette/CanvasLayer
+@onready var menu: CanvasLayer = $Menu
+@onready var camera_2d: Camera2D = $Player/Camera2D
 
 func _ready() -> void:
 	# Turn on visible
 	parallax_background.visible = true
 	tile_map_layer_background.visible = true
-	hud.visible = true
+	hud.visible = not menu.visible
+	camera_2d.offset.y = (get_viewport().size.y * -1 if menu.visible else -60.0)
 	canvas_layer.visible = true
 	
 	# Turn off visible
